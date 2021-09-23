69b7d6e9065196b0951438aed84342d9b4a82af0
Samuel Yeung <samuelyeung.hk@gmail.com>
2021-09-23 02:28:06 -0400

    demo compress and decompress

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,10 +1,72 @@
 package com.example.demo;
 
+import com.github.luben.zstd.Zstd;
+import org.apache.commons.io.IOUtils;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("reading......");
+        String s = readFile();
+
+        System.out.println("compressing......");
+        compress(s);
+
+        System.out.println("decompressing......");
+        byte[] buffer = readCompressedFile();
+        long decompressedSize = Zstd.decompressedSize(buffer);
+        byte[] decompressedBytes = Zstd.decompress(buffer, (int) decompressedSize);
+        String decompressedStr = new String(decompressedBytes, StandardCharsets.UTF_8);
+        String path = HelloApplication.class.getResource("/").getPath() + File.separator + "decompressed.txt";
+        File f = new File(path);
+        System.out.println("decompressed path = " + f.getAbsolutePath());
+        FileWriter fw = new FileWriter(f);
+        IOUtils.write(decompressedStr, fw);
+        fw.close();
+
+        System.out.println("DONE");
     } // end main
+
+    private static byte[] readCompressedFile() throws IOException {
+        String compressedFilePath = HelloApplication.class.getResource("/compressed.bin").getPath();
+        int size = (int) Files.size(Paths.get(compressedFilePath));
+        InputStream is2 = HelloApplication.class.getResourceAsStream("/compressed.bin");
+        byte[] buffer = new byte[size];
+        IOUtils.read(is2, buffer);
+        is2.close();
+        return buffer;
+    }
+
+    private static String readFile() throws IOException {
+        InputStream is = HelloApplication.class.getResourceAsStream("/sample.txt");
+        String s = IOUtils.toString(is, Charset.defaultCharset());
+        is.close();
+        return s;
+    }
+
+    private static void compress(String s) throws IOException {
+        byte[] b = Zstd.compress(s.getBytes(StandardCharsets.UTF_8));
+        String outputPath = HelloApplication.class.getResource("/").getPath() + File.separator + "compressed.bin";
+        File outputFile = new File(outputPath);
+        System.out.println(outputFile.getAbsolutePath());
+        FileOutputStream f = new FileOutputStream(outputFile);
+        f.write(b);
+        f.flush();
+        f.close();
+    }
 } // end class
